9452ef6cb87ff8c2b41cac9d6be5bf0d481768b3
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-19 18:12:47 +0000

    automatically backed up by learn

---
+++ db/migrate/02_try_again.rb
@@ -0,0 +1,16 @@
+class CreateStudents < ActiveRecord::Migration[5.1]
+  def up
+  end
+
+  def down
+  end
+  
+  def change
+    if !data_source_exists?(:students)
+      create_table :students do |x|
+        x.string :name
+      end
+    end
+  end
+
+end
